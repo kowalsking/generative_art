9293de59e868f3a8d8ed5326d36e110f073593cb
Mityai Kowalksi <kovvalsking@gmail.com>
2019-10-01 22:10:23 +0300

    joy division;

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-// Tiled Lines
+/*
+    TILED LINES
+*/
 
-function tiledLines() {
+// function tiledLines() {
+//     const canvas = document.querySelector('canvas');
+//     const text = document.querySelector('#rgb-text');
+//     const ctx = canvas.getContext('2d');
+
+//     const size = 400;
+//         // window.innerWidth/2;
+//     const dpr = window.devicePixelRatio;
+
+//     canvas.width = size * dpr;
+//     canvas.height = size * dpr;
+
+//     ctx.scale(dpr, dpr);
+//     ctx.lineWidth = 2;
+
+//     let step = 15;
+//     let clr = randRGB();
+
+//     for (let x = 0; x < size; x += step) {
+//         for (let y = 0; y < size; y += step) {
+//             ctx.fillRect(0,0, size, size);
+//             ctx.fillStyle = clr;
+//             draw(x, y, step, step);
+//         }
+//     }
+
+//     function draw(x, y, width, height, color) {
+//         let leftToRight = Math.random() >= 0.5;
+//         if (leftToRight) {
+//             ctx.moveTo(x, y);
+//             ctx.lineTo(x + width, y + height);
+//         } else {
+//             ctx.moveTo(x + width, y);
+//             ctx.lineTo(x, y + height);
+//         }
+//         ctx.stroke();
+//     }
+
+//     function randRGB() {
+//         let r,g,b,rgb;
+//         r = Math.floor(Math.random()*256);
+//         g = Math.floor(Math.random()*256);
+//         b = Math.floor(Math.random()*256);
+//         rgb = `rgb(${r},${g},${b})`;
+//         text.textContent = rgb;
+//         return rgb;
+//     }
+// }
+
+
+/*
+    JOY DIVISION
+*/
+
+function joyDivision() {
     const canvas = document.querySelector('canvas');
     const text = document.querySelector('#rgb-text');
     const ctx = canvas.getContext('2d');
@@ -12,41 +68,49 @@ function tiledLines() {
     canvas.width = size * dpr;
     canvas.height = size * dpr;
 
-    ctx.scale(dpr, dpr);
-    ctx.lineWidth = 2;
+    const step = 10;
+    const lines = [];
 
-    let step = 15;
-    let clr = randRGB();
+    // create the lines
 
-    for (let x = 0; x < size; x += step) {
-        for (let y = 0; y < size; y += step) {
-            ctx.fillRect(0,0, size, size);
-            ctx.fillStyle = clr;
-            draw(x, y, step, step);
+    for (let i = 0; i <= size - step; i += step) {
+        const line = [];
+        for (let j = 0; j <= size - step; j += step) {
+            let distanceToCenter = Math.abs(j - size / 2);
+            let variance = Math.max(size / 2 - 50 - distanceToCenter, 0);
+            let random = Math.random() * variance / 2 * -1;
+            let point = {x: j, y: i + random};
+            line.push(point);
         }
+        lines.push(line);
     }
 
-    function draw(x, y, width, height, color) {
-        let leftToRight = Math.random() >= 0.5;
-        if (leftToRight) {
-            ctx.moveTo(x, y);
-            ctx.lineTo(x + width, y + height);
-        } else {
-            ctx.moveTo(x + width, y);
-            ctx.lineTo(x, y + height);
+    // do the drawing
+    
+    for (let i = 0; i < lines.length; i ++) {
+        ctx.beginPath();
+        console.log(lines[0]);
+        ctx.moveTo(lines[i][0].x, lines[i][0].y);
+
+        for (let j = 0; j < lines[i].length; j ++) { 
+            ctx.lineTo(lines[i][j].x, lines[i][j].y) 
         }
         ctx.stroke();
     }
+    ctx.save();
+    ctx.globalCompositeOperation = 'destination-out';
+    ctx.fill();
+    ctx.restore();
 
-    function randRGB() {
-        let r,g,b,rgb;
-        r = Math.floor(Math.random()*256);
-        g = Math.floor(Math.random()*256);
-        b = Math.floor(Math.random()*256);
-        rgb = `rgb(${r},${g},${b})`;
-        text.textContent = rgb;
-        return rgb;
-    }
+    for(var j = 0; j < lines[i].length - 2; j++) {
+        var xc = (lines[i][j].x + lines[i][j + 1].x) / 2;
+        var yc = (lines[i][j].y + lines[i][j + 1].y) / 2;
+        context.quadraticCurveTo(lines[i][j].x, lines[i][j].y, xc, yc);
+      }
+      
+      context.quadraticCurveTo(lines[i][j].x, lines[i][j].y, lines[i][j + 1].x, lines[i][j + 1].y);
+
+
 }
 
-document.addEventListener('DOMContentLoaded', tiledLines);
+document.addEventListener('DOMContentLoaded', joyDivision);
