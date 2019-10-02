b0bd7a94b58ec29216c1172685c156cbce0263a1
Kozlenko <d.kozlenko@mediawayss.com>
2019-10-02 12:31:42 +0300

    joy division final

--- index.js
+++ index.js
@@ -2,57 +2,54 @@
     TILED LINES
 */
 
+function tiledLines() {
+    const canvas = document.querySelector('canvas');
+    const text = document.querySelector('#rgb-text');
+    const ctx = canvas.getContext('2d');
 
-// function tiledLines() {
-//     console.log('This is generation art.\n Tiled Lines.');
-// function tiledLines() {
-//     const canvas = document.querySelector('canvas');
-//     const text = document.querySelector('#rgb-text');
-//     const ctx = canvas.getContext('2d');
+    const size = 400;
+        // window.innerWidth/2;
+    const dpr = window.devicePixelRatio;
 
-//     const size = 400;
-//         // window.innerWidth/2;
-//     const dpr = window.devicePixelRatio;
+    canvas.width = size * dpr;
+    canvas.height = size * dpr;
 
-//     canvas.width = size * dpr;
-//     canvas.height = size * dpr;
+    ctx.scale(dpr, dpr);
+    ctx.lineWidth = 2;
 
-//     ctx.scale(dpr, dpr);
-//     ctx.lineWidth = 2;
+    let step = 15;
+    let clr = randRGB();
 
-//     let step = 15;
-//     let clr = randRGB();
+    for (let x = 0; x < size; x += step) {
+        for (let y = 0; y < size; y += step) {
+            ctx.fillRect(0,0, size, size);
+            ctx.fillStyle = clr;
+            draw(x, y, step, step);
+        }
+    }
 
-//     for (let x = 0; x < size; x += step) {
-//         for (let y = 0; y < size; y += step) {
-//             ctx.fillRect(0,0, size, size);
-//             ctx.fillStyle = clr;
-//             draw(x, y, step, step);
-//         }
-//     }
+    function draw(x, y, width, height, color) {
+        let leftToRight = Math.random() >= 0.5;
+        if (leftToRight) {
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + width, y + height);
+        } else {
+            ctx.moveTo(x + width, y);
+            ctx.lineTo(x, y + height);
+        }
+        ctx.stroke();
+    }
 
-//     function draw(x, y, width, height, color) {
-//         let leftToRight = Math.random() >= 0.5;
-//         if (leftToRight) {
-//             ctx.moveTo(x, y);
-//             ctx.lineTo(x + width, y + height);
-//         } else {
-//             ctx.moveTo(x + width, y);
-//             ctx.lineTo(x, y + height);
-//         }
-//         ctx.stroke();
-//     }
-
-//     function randRGB() {
-//         let r,g,b,rgb;
-//         r = Math.floor(Math.random()*256);
-//         g = Math.floor(Math.random()*256);
-//         b = Math.floor(Math.random()*256);
-//         rgb = `rgb(${r},${g},${b})`;
-//         text.textContent = rgb;
-//         return rgb;
-//     }
-// }
+    function randRGB() {
+        let r,g,b,rgb;
+        r = Math.floor(Math.random()*256);
+        g = Math.floor(Math.random()*256);
+        b = Math.floor(Math.random()*256);
+        rgb = `rgb(${r},${g},${b})`;
+        text.textContent = rgb;
+        return rgb;
+    }
+}
 
 
 /*
@@ -112,8 +109,6 @@ function joyDivision() {
             ctx.stroke();
         }
     }
-
-
 }
 
 document.addEventListener('DOMContentLoaded', joyDivision);
