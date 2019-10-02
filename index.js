8c5a6ada4e2408f0f803d01b2fc29b7c9560991b
Kozlenko <d.kozlenko@mediawayss.com>
2019-10-02 12:15:30 +0300

    joy division

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
     TILED LINES
 */
 
+
+// function tiledLines() {
+//     console.log('This is generation art.\n Tiled Lines.');
 // function tiledLines() {
 //     const canvas = document.querySelector('canvas');
 //     const text = document.querySelector('#rgb-text');
@@ -71,44 +74,44 @@ function joyDivision() {
     const step = 10;
     const lines = [];
 
-    // create the lines
+    createLines();
+    draw();
 
-    for (let i = 0; i <= size - step; i += step) {
-        const line = [];
-        for (let j = 0; j <= size - step; j += step) {
-            let distanceToCenter = Math.abs(j - size / 2);
-            let variance = Math.max(size / 2 - 50 - distanceToCenter, 0);
-            let random = Math.random() * variance / 2 * -1;
-            let point = {x: j, y: i + random};
-            line.push(point);
+    // create the lines
+    function createLines() {
+        for (let i = step; i <= size - step; i += step) {
+            const line = [];
+            for (let j = step; j <= size - step; j += step) {
+                let distanceToCenter = Math.abs(j - size / 2);
+                let variance = Math.max(size / 2 - 50 - distanceToCenter, 0);
+                let random = Math.random() * variance / 2 * -1;
+                let point = {x: j, y: i + random};
+                line.push(point);
+            }
+            lines.push(line);
         }
-        lines.push(line);
     }
 
     // do the drawing
-    
-    for (let i = 0; i < lines.length; i ++) {
-        ctx.beginPath();
-        console.log(lines[0]);
-        ctx.moveTo(lines[i][0].x, lines[i][0].y);
+    function draw() {
+        for (let i = 5; i < lines.length-4; i++) {
+            ctx.beginPath();
+            ctx.moveTo(lines[i][0].x, lines[i][0].y);
 
-        for (let j = 0; j < lines[i].length; j ++) { 
-            ctx.lineTo(lines[i][j].x, lines[i][j].y) 
+            for (var j = 0; j < lines[i].length - 2; j++) {
+                var xc = (lines[i][j].x + lines[i][j + 1].x) / 2;
+                var yc = (lines[i][j].y + lines[i][j + 1].y) / 2;
+                ctx.quadraticCurveTo(lines[i][j].x, lines[i][j].y, xc, yc);
+            }
+
+            ctx.quadraticCurveTo(lines[i][j].x, lines[i][j].y, lines[i][j + 1].x, lines[i][j + 1].y);
+            ctx.save();
+            ctx.globalCompositeOperation = 'destination-out';
+            ctx.fill();
+            ctx.restore();
+            ctx.stroke();
         }
-        ctx.stroke();
     }
-    ctx.save();
-    ctx.globalCompositeOperation = 'destination-out';
-    ctx.fill();
-    ctx.restore();
-
-    for(var j = 0; j < lines[i].length - 2; j++) {
-        var xc = (lines[i][j].x + lines[i][j + 1].x) / 2;
-        var yc = (lines[i][j].y + lines[i][j + 1].y) / 2;
-        context.quadraticCurveTo(lines[i][j].x, lines[i][j].y, xc, yc);
-      }
-      
-      context.quadraticCurveTo(lines[i][j].x, lines[i][j].y, lines[i][j + 1].x, lines[i][j + 1].y);
 
 
 }
