2617400794cb8c27609e8e1cff99742f8b93a923
Kowalsking <kovvalsking@gmail.com>
2019-10-03 11:58:45 +0300

    cubic disarray

--- index.js
+++ index.js
@@ -55,59 +55,117 @@ function tiledLines() {
     JOY DIVISION
 */
 
-function joyDivision() {
+// function joyDivision() {
+//     const canvas = document.querySelector('canvas');
+//     const text = document.querySelector('#rgb-text');
+//     const ctx = canvas.getContext('2d');
+//
+//     const size = 400;
+//         // window.innerWidth/2;
+//     const dpr = window.devicePixelRatio;
+//
+//     canvas.width = size * dpr;
+//     canvas.height = size * dpr;
+//
+//     const step = 10;
+//     const lines = [];
+//
+//     createLines();
+//     draw();
+//
+//     // create the lines
+//     function createLines() {
+//         for (let i = step; i <= size - step; i += step) {
+//             const line = [];
+//             for (let j = step; j <= size - step; j += step) {
+//                 let distanceToCenter = Math.abs(j - size / 2);
+//                 let variance = Math.max(size / 2 - 50 - distanceToCenter, 0);
+//                 let random = Math.random() * variance / 2 * -1;
+//                 let point = {x: j, y: i + random};
+//                 line.push(point);
+//             }
+//             lines.push(line);
+//         }
+//     }
+//
+//     // do the drawing
+//     function draw() {
+//         for (let i = 5; i < lines.length-4; i++) {
+//             ctx.beginPath();
+//             ctx.moveTo(lines[i][0].x, lines[i][0].y);
+//
+//             for (var j = 0; j < lines[i].length - 2; j++) {
+//                 var xc = (lines[i][j].x + lines[i][j + 1].x) / 2;
+//                 var yc = (lines[i][j].y + lines[i][j + 1].y) / 2;
+//                 ctx.quadraticCurveTo(lines[i][j].x, lines[i][j].y, xc, yc);
+//             }
+//
+//             ctx.quadraticCurveTo(lines[i][j].x, lines[i][j].y, lines[i][j + 1].x, lines[i][j + 1].y);
+//             ctx.save();
+//             ctx.globalCompositeOperation = 'destination-out';
+//             ctx.fill();
+//             ctx.restore();
+//             ctx.stroke();
+//         }
+//     }
+// }
+
+/*
+   CUBIC DISARRAY
+*/
+
+function cubicDisarray() {
     const canvas = document.querySelector('canvas');
     const text = document.querySelector('#rgb-text');
     const ctx = canvas.getContext('2d');
 
-    const size = 400;
-        // window.innerWidth/2;
+    const size = 320;
+    // window.innerWidth/2;
     const dpr = window.devicePixelRatio;
 
     canvas.width = size * dpr;
     canvas.height = size * dpr;
 
-    const step = 10;
-    const lines = [];
+    ctx.lineWidth = 2;
+    ctx.scale(dpr, dpr);
 
-    createLines();
-    draw();
+    let squareSize = 30;
+    let randomDisplacement = 15;
+    let rotateMultiplier = 20;
+    let offset = 10;
 
-    // create the lines
-    function createLines() {
-        for (let i = step; i <= size - step; i += step) {
-            const line = [];
-            for (let j = step; j <= size - step; j += step) {
-                let distanceToCenter = Math.abs(j - size / 2);
-                let variance = Math.max(size / 2 - 50 - distanceToCenter, 0);
-                let random = Math.random() * variance / 2 * -1;
-                let point = {x: j, y: i + random};
-                line.push(point);
-            }
-            lines.push(line);
-        }
+    function draw(width, height) {
+        ctx.beginPath();
+        ctx.rect(-width/2, -height/2, width, height);
+        ctx.fillStyle = randRGB();
+        ctx.fillRect(-width/2, -height/2, width, height);
+        ctx.stroke();
     }
 
-    // do the drawing
-    function draw() {
-        for (let i = 5; i < lines.length-4; i++) {
-            ctx.beginPath();
-            ctx.moveTo(lines[i][0].x, lines[i][0].y);
+    function randRGB() {
+        let r,g,b,rgb;
+        r = Math.floor(Math.random()*256);
+        g = Math.floor(Math.random()*256);
+        b = Math.floor(Math.random()*256);
+        rgb = `rgb(${r},${g},${b})`;
+        // text.textContent = rgb;
+        return rgb;
+    }
 
-            for (var j = 0; j < lines[i].length - 2; j++) {
-                var xc = (lines[i][j].x + lines[i][j + 1].x) / 2;
-                var yc = (lines[i][j].y + lines[i][j + 1].y) / 2;
-                ctx.quadraticCurveTo(lines[i][j].x, lines[i][j].y, xc, yc);
-            }
+    for (let i = squareSize; i <= size - squareSize ; i+=squareSize) {
+        for (let j = 10; j <= size - squareSize; j+=squareSize) {
+            let plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+            let rotateAmt = j / size * Math.PI / 180 * plusOrMinus * Math.random() * rotateMultiplier;
+            plusOrMinus = Math.random() < 0.5 ? -1 : 1;
+            let translateAmt = j / size * plusOrMinus * Math.random() * randomDisplacement;
 
-            ctx.quadraticCurveTo(lines[i][j].x, lines[i][j].y, lines[i][j + 1].x, lines[i][j + 1].y);
             ctx.save();
-            ctx.globalCompositeOperation = 'destination-out';
-            ctx.fill();
+            ctx.translate(i + translateAmt + offset, j + offset);
+            ctx.rotate(rotateAmt);
+            draw(squareSize, squareSize);
             ctx.restore();
-            ctx.stroke();
         }
     }
 }
 
-document.addEventListener('DOMContentLoaded', joyDivision);
+document.addEventListener('DOMContentLoaded', cubicDisarray);
